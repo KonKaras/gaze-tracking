aef64dd39b3a117401fa2f09695a41ffee97d6b2
Konstantin Karas <konstantin.karas@tum.de>
2020-12-28 11:25:46 +0100

    initial project setup, includes magic leap sample project

---
+++ Assets/Scripts/Tracking/EyeTracking.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class EyeTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MLEyes.Start();
+        StartCoroutine(WriteData());
+    }
+
+    void OnDisable()
+    {
+        MLEyes.Stop();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (MLEyes.IsStarted)
+        {
+            
+        }
+    }
+
+    IEnumerator WriteData()
+    {
+        yield return new WaitForSeconds(.2f);
+    }
+
+}
