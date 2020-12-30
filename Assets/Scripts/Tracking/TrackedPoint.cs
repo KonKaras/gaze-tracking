cde10b46ce3c1f47aa6194c918def6d10a456e63
Konstantin Karas <konstantin.karas@tum.de>
2020-12-30 11:21:42 +0100

    added dummy point generation, started with json

---
+++ Assets/Scripts/Tracking/TrackedPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class TrackedPoint
+{
+    public Vector3 pos;
+    public float time;
+}
+
+[Serializable]
+public class DataPoints
+{
+    public List<TrackedPoint> points;
+    public DataPoints()
+    {
+        points = new List<TrackedPoint>();
+    }
+}
