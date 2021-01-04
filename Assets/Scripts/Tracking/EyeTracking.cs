d7b0d8e1fb03006d3fdf26c7c8be012463684bda
Konstantin Karas <konstantin.karas@tum.de>
2021-01-04 17:17:01 +0100

    basic json save/load functionality

--- Assets/Scripts/Tracking/EyeTracking.cs
+++ Assets/Scripts/Tracking/EyeTracking.cs
@@ -1,34 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.MagicLeap;
+using UnityEditor;
+using System.IO;
+//using UnityEngine.XR.MagicLeap;
 
 public class EyeTracking : MonoBehaviour
 {
+    public bool shouldRecord;
+    bool recording;
+    bool savingDone;
+    string saved ="";
+    public string path = "Assets/Scripts/Tracking/tracking.txt";
+
+    DataPoints dataPoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        MLEyes.Start();
-        StartCoroutine(WriteData());
+        dataPoints = new DataPoints();
+        if (shouldRecord)
+        {
+            //MLEyes.Start();
+        }
     }
 
     void OnDisable()
     {
-        MLEyes.Stop();
+        //MLEyes.Stop();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (MLEyes.IsStarted)
+        if (shouldRecord && !recording) //&& MLEyes.IsStarted)
         {
-            
+            StartCoroutine("WriteData");
+        }
+        else
+        {
+            if (!shouldRecord && !savingDone)
+            {
+                StopCoroutine("WriteData");
+                SaveAsJson();
+                List<TrackedPoint> points = LoadFromJson();
+                foreach(TrackedPoint p in points)
+                {
+                    Debug.Log(p.time + ": " + p.pos);
+                }
+            }
         }
     }
 
     IEnumerator WriteData()
     {
-        yield return new WaitForSeconds(.2f);
+        savingDone = false;
+        recording = true;
+        TrackedPoint point = new TrackedPoint();
+        point.pos = new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), Random.Range(-2f, 2f));
+        point.time = Time.time;
+        dataPoints.points.Add(point);
+        yield return new WaitForSeconds(.5f);
+        saved = JsonUtility.ToJson(dataPoints);
+        recording = false;
     }
 
+    void SaveAsJson()
+    {
+        //save joint settings
+        string values = "";
+        foreach (TrackedPoint pt in dataPoints.points)
+        {
+            values += JsonUtility.ToJson(pt) + "\n";
+        }
+        values = values.Substring(0, values.Length - 1);
+
+        savingDone = true;
+
+        File.WriteAllText(path, values);
+        AssetDatabase.ImportAsset(path);
+    }
+
+    List<TrackedPoint> LoadFromJson()
+    {
+        StreamReader reader = new StreamReader(path);
+        string savedPoints = reader.ReadToEnd();
+
+        List<TrackedPoint> recoveredPoints = new List<TrackedPoint>();
+        string[] trackingEntries = savedPoints.Split('\n');
+        foreach(string entry in trackingEntries)
+        {
+            TrackedPoint pt = JsonUtility.FromJson<TrackedPoint>(entry);
+            recoveredPoints.Add(pt);
+        }
+
+        return recoveredPoints;
+    }
 }
