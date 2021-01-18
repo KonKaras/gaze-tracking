8c399f6128d5c2f5ffb1d8f3ca27c7d9935e3ba5
Markus Hamberger <ga42mod@mytum.de>
2021-01-18 13:42:35 +0100

    added Range Time Slider

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -8,6 +8,9 @@ public class UI : MonoBehaviour
     public List<GameObject> points;
     public List<TrackedPoint> pointsData;
     public float maxTime;
+
+    private float minSliderValue = 0;
+    private float maxSliderValue = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,5 +44,34 @@ public class UI : MonoBehaviour
             }
         
         }
+    }
+
+    public void HandleTimeRange()
+    {
+        float maxTimeValue = maxSliderValue * maxTime;
+        float minTimeValue = minSliderValue * maxTime;
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            if (pointsData[i].time <= maxTimeValue && pointsData[i].time >= minTimeValue)
+            {
+                points[i].SetActive(true);
+            }
+            else
+            {
+                points[i].SetActive(false);
+            }
+
+        }
+    }
+
+    public void setMinSliderValue (float value)
+    {
+        minSliderValue = value;
+    }
+
+    public void setMaxSliderValue(float value)
+    {
+        maxSliderValue = value;
     }
 }
