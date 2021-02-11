0ad382680f783f4b841d2f560eeccd97d764782e
Markus Hamberger <ga42mod@mytum.de>
2021-02-11 15:08:30 +0100

    added rangeSlider

---
+++ Assets/Scripts/UI/MaximumSlider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MaximumSlider : Slider
+{
+	public MinimumSlider minSlider;
+
+	protected override void Set(float input, bool sendCallback)
+	{
+
+		if (input <= minSlider.value)
+		{
+			return;
+		}
+
+		base.Set(input, sendCallback);
+	}
+}
